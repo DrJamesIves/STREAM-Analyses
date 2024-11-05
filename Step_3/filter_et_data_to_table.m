331b3c2b2078ae73914fea8f0fc65a4dbe29e5fe
DrJamesIves <james.ernest.ives@gmail.com>
2024-11-05 12:18:52 +0000

    Update filter_et_data_to_table.m
    Updates to allow for multiple trials and to split the social/non-social conditions in the resting state video condition.

--- Step_3/filter_et_data_to_table.m
+++ Step_3/filter_et_data_to_table.m
@@ -5,13 +5,13 @@ function filter_et_data_to_table
 % Released under GNU GPL v3.0: https://www.gnu.org/licenses/gpl-3.0.html
 % Open to collaboration—feel free to contact me!
 
-% The purpose of this function is to take in a root folder with et info files in, load 
+% The purpose of this function is to take in a root folder with et info files in, load
 % them in and filter the resulting tab object for the relevant info for analysis.
 
 % Expected that this is the epoched_info_mat folder from ET epoching
-root = 'E:\Birkbeck\STREAM\Datasets\2. Preprocessed';
-root_path = fullfile(root, '2.1.1 Epoched_ET\epoched_info_mat');
-out_path = fullfile(root, 'Overview data\');
+root = 'E:\Birkbeck\STREAM INDIA\Datasets\';
+root_path = fullfile(root, '2. Preprocessed\2.1.1 Epoched_ET\epoched_info_mat');
+out_path = fullfile(root, '3. Analysed\3.3 ET Overview Data\');
 out_path_csv = fullfile(out_path, 'csv');
 out_path_matlab = fullfile(out_path, 'mat');
 
@@ -36,7 +36,7 @@ for file = 1:length(files)
     if startsWith(files(file).name, '.') | ~endsWith(files(file).name, '.mat')
         continue
     end
-    
+
     % Split the filename into its constituent parts.
     name_parts = split(files(file).name, '_');
     ppt_no = name_parts{3};
@@ -77,9 +77,9 @@ for file = 1:length(files)
     % Load the file, should give a tab file
     load(fullfile(files(file).folder, files(file).name))
 
-
     % Filter out the relevant columns
     eyeT_info.id = tab.id;
+    eyeT_info.attempt = repmat(attempt, size(tab, 1), 1);
     eyeT_info.timestamp = tab.timestamp;
     eyeT_info.(onset_name) = tab.(onset_name);
     eyeT_info.(offset_name) = tab.(offset_name);
@@ -101,26 +101,79 @@ for file = 1:length(files)
             eyeT_info.fs_right = tab.fs_right;
             eyeT_info.stim_type_left = tab.stim_type_left;
             eyeT_info.stim_type_right = tab.stim_type_right;
+        case 'restingvideos2'
+            eyeT_info.auditorygating = tab.auditorygatingvideochoice;
+            eyeT_info.key = tab.key;
+
+            % As we have to split by condition and save we will do this here rather than in the main loop
+            % Matlab file
+            eyeT_info2 = eyeT_info;
+            eyeT_info = eyeT_info(startsWith(eyeT_info.key, 'FACE'), :);
+
+            if ~isempty(eyeT_info)
+                new_fname = [files(file).name(1:end-9), '_social_info'];
+
+                save(fullfile(out_path_matlab, [new_fname, '.mat']), 'eyeT_info');
+                writetable(eyeT_info, fullfile(out_path_csv, [new_fname, '.csv']));
+
+                % Add to the "all" info table which can be saved and filtered later
+                eyeT_info_all = [eyeT_info_all; ...
+                    table(eyeT_info.id(1), ...
+                    str2num(attempt), ...
+                    {'Rest_Vid_Social'}, ...
+                    mean(eyeT_info.nan_percentage, 'omitnan'), ...
+                    mean(eyeT_info.x_mean, 'omitnan'), ...
+                    mean(eyeT_info.y_mean, 'omitnan'), ...
+                    mean(eyeT_info.x_y_sdd, 'omitnan'), ...
+                    mean([eyeT_info.et_look_proportion{:}], 'omitnan'))];
+            end
+
+            % Now repeat for the non-social
+            % Matlab file
+            eyeT_info = eyeT_info2;
+            eyeT_info = eyeT_info(startsWith(eyeT_info.key, 'TOY'), :);
+
+            if ~isempty(eyeT_info)
+                new_fname = [files(file).name(1:end-9), '_non-social_info'];
+
+                save(fullfile(out_path_matlab, [new_fname, '.mat']), 'eyeT_info');
+                writetable(eyeT_info, fullfile(out_path_csv, [new_fname, '.csv']));
+
+                % Add to the "all" info table which can be saved and filtered later
+                eyeT_info_all = [eyeT_info_all; ...
+                    table(eyeT_info.id(1), ...
+                    str2num(attempt), ...
+                    {'Rest_Vid_Non-Social'}, ...
+                    mean(eyeT_info.nan_percentage, 'omitnan'), ...
+                    mean(eyeT_info.x_mean, 'omitnan'), ...
+                    mean(eyeT_info.y_mean, 'omitnan'), ...
+                    mean(eyeT_info.x_y_sdd, 'omitnan'), ...
+                    mean([eyeT_info.et_look_proportion{:}], 'omitnan'))];
+            end
+
     end
 
-    % Matlab files
-    save(fullfile(out_path_matlab, files(file).name), 'eyeT_info');
-    writetable(eyeT_info, fullfile(out_path_csv, [files(file).name(1:end-4), '.csv']));
+    if ~strcmp(condition, 'restingvideos2')
+        % Matlab files
+        save(fullfile(out_path_matlab, files(file).name), 'eyeT_info');
+        writetable(eyeT_info, fullfile(out_path_csv, [files(file).name(1:end-4), '.csv']));
 
-    % Add to the "all" info table which can be saved and filtered later
-    eyeT_info_all = [eyeT_info_all; ...
-        table(tab.id(1), ...
-        {condition}, ...
-        mean(tab.nan_percentage, 'omitnan'), ...
-        mean(tab.x_mean, 'omitnan'), ...
-        mean(tab.y_mean, 'omitnan'), ...
-        mean(tab.x_y_sdd, 'omitnan'), ...
-        mean([tab.et_looking_prop{:}], 'omitnan'))];
+        % Add to the "all" info table which can be saved and filtered later
+        eyeT_info_all = [eyeT_info_all; ...
+            table(tab.id(1), ...
+            str2num(attempt), ...
+            {condition}, ...
+            mean(tab.nan_percentage, 'omitnan'), ...
+            mean(tab.x_mean, 'omitnan'), ...
+            mean(tab.y_mean, 'omitnan'), ...
+            mean(tab.x_y_sdd, 'omitnan'), ...
+            mean([tab.et_looking_prop{:}], 'omitnan'))];
+    end
 
 end
 
 % Set the headings on the all table
-header={'id', 'task', 'nan_percentage','x_mean', 'y_mean', 'x_y_sdd', 'look_proportion'};
+header={'id', 'attempt', 'task', 'nan_percentage','x_mean', 'y_mean', 'x_y_sdd', 'look_proportion'};
 eyeT_info_all.Properties.VariableNames = header;
 
 % Save info files on a trial by trial basis
